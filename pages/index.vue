108c50aef638f540f18666845c2d32e4ffa68d1f
Mio Nekoya <admin@mio9.sh>
2024-01-08 20:27:33 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- page container -->
-  <div class="bg-slate-900 text-white p-3 w-screen h-screen font-mono">
+  <div class="bg-slate-900 text-white py-3 px-5 w-screen h-screen font-mono">
     <!-- the tabbybox, don't touch -->
     <pre style="line-height: 1;">
   ^ ^________
@@ -9,8 +9,12 @@
 | >_       | /  ░█░ █▀█ █▄█ █▄█ ░█░ █▄█ █▄█ █░█
 +----------+       
   </pre>
-    <hr class="my-2" />
+    <!-- Navbar -->
     <nav>
+      $ <span class="text-green-300">ls </span><br/>
+      <a href="https://tabbybox.art" class="mr-2 text-teal-400">home</a>
+      <a href="https://tabbybox.art/about" class="mx-2 text-blue-300">lo-res</a>
+      <a href="https://github.com/tabbybox" class="mx-2 text-blue-300">repo</a>
       <!-- <span class="mx-2 my-1">
         "home"
       </span>
@@ -19,13 +23,36 @@
       </span> -->
 
     </nav>
-    <p>{{ quote() }}</p>
+    <hr class="my-2" />
+
+    <!-- Actual content goes here -->
+    <!-- <ClientOnly>
+      <p>{{ quote() }}</p>
+    </ClientOnly> -->
+
+    <span class="text-4xl absolute animate-ping">We make loud music</span>
+    <span class="text-4xl relative">We make loud music</span>
+
+
+    <!-- <p class="h-1 w-1 animate-spin">e</p>
+     -->
+
+     <div class="w-screen flex justify-center items-center">
+      <img src="~/assets/sun.svg" alt="" class="w-40 h-40 animate-spin">
+     </div>
+
+
+
+    <!-- Footer -->
+    <div class="fixed bottom-0 left-0 w-full text-center py-3">
+      <span class="mx-2 text-xs text-slate-400">tabbybox.art (c) 2024 </span>
+    </div>
   </div>
 </template>
 <script setup>
 const quotePool = [
   'hello, i like dubstep.',
-  'headshot GG noob',
+  'headshot GG noob!',
   'i.e., the tissuebox',
   'we make loud music',
   'under heavy construction work',
