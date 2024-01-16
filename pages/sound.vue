90efe1086c9c21531f1ac0fb25be9dce9fbd06c8
Mio Nekoya <admin@mio9.sh>
2024-01-16 22:57:24 +0800

    changed nothing

--- pages/sound.vue
+++ pages/sound.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-       Welcome to Lo-RES Division. We really make loud music. We make really loud music.
+        Welcome to Lo-RES Division. We really make loud music. We make really loud music.
     </div>
 </template>
