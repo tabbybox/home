5dab67ef97ca59e9537d41369ce6c9ed5f8ba7f4
Mio Nekoya <admin@mio9.sh>
2024-01-16 21:34:05 +0800

    Update layout, better bsb

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtPage/>
+  <div class="">
+    <NuxtPage />
+    <div class="fixed bottom-0 left-0 w-full text-center py-3">
+      <span class="mx-2 text-xs text-slate-400">tabbybox.art (c) 2024 </span>
+    </div>
   </div>
 </template>
